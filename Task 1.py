9755603659c2186176bcc3fccbf91ce0bc02922b
Vlad20003 <lolipop4ikq@gmail.com>
2025-03-03 23:34:52 +0300

    Лабораторная работа (2сем) 2

--- Task 1.py
+++ Task 1.py
@@ -1,19 +1,36 @@
-# TODO Напишите функцию для поиска индекса товара
-def find_index(list_, item_):
-    a = 0
-    for i in list_:
-        if i == item_:
-            a = list_.index(i)
-            return a
-
-items_list = ['яблоко', 'банан', 'апельсин', 'груша', 'киви', 'банан']
-
-for find_item in ['банан', 'груша', 'персик']:
-    index_item = find_index(items_list, find_item)  # TODO Вызовите функцию, что получить индекс товара
-    if index_item is not None:
-        print(f"Первое вхождение товара '{find_item}' имеет индекс {index_item}.")
-    else:
-        print(f"Товар '{find_item}' не найден в списке.")
+BOOKS_DATABASE = [
+    {
+        "id": 1,
+        "name": "test_name_1",
+        "pages": 200,
+    },
+    {
+        "id": 2,
+        "name": "test_name_2",
+        "pages": 400,
+    }
+]
 
 
+class Book:
+    def __init__(self, id_, name, pages):
+        self.id = id_
+        self.name = name
+        self.pages = pages
 
+    def __str__(self):
+        return f'Книга "{self.name}"'
+
+    def __repr__(self):
+        return f'Book(id_={self.id}, name={repr(self.name)}, pages={self.pages})'
+
+
+if __name__ == '__main__':
+    # инициализируем список книг
+    list_books = [
+        Book(id_=book_dict["id"], name=book_dict["name"], pages=book_dict["pages"]) for book_dict in BOOKS_DATABASE
+    ]
+    for book in list_books:
+        print(book)  # проверяем метод __str__
+
+    print(list_books)  # проверяем метод __repr__
